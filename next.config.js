70a3019397cb7d311387211b72a5ac40e0d2b416
Victor Procure <victor@victorprocure.co>
2018-08-30 12:53:30 -0600

    Restarting

--- next.config.js
+++
@@ -1,5 +0,0 @@
-const withTypescript = require('@zeit/next-typescript');
-const withCss = require('@zeit/next-css');
-const withImages = require('next-images');
-
-module.exports =  withTypescript(withCss(withImages()));
